f892b8cb264075303627d316db5f9510b32d990a
Eddy Hood <eddy@ignitespot.com>
2022-06-09 06:30:32 -0600

    Remove code for testing html, css, bootstrap, and image setup

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,3 @@
 'use strict';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../css/style.css';
-import smallLogo from '../img/stoicard-logo-small.png';
-
-const logoContainer = document.getElementById('logo');
-const logo = new Image();
-logo.src = smallLogo;
-logoContainer.appendChild(logo);
