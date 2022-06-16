b4022d074759850b4d16df032896627c84639e06
Eddy Hood <eddy@ignitespot.com>
2022-06-16 10:44:22 -0600

    Add feature cards on home page

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,8 @@
 'use strict';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import '../css/style.css';
 import html from '../html/template-main.html'  //for html loader to grab img tags
+
 
 
